37349d0b3801d6a60576d1eaf786f94b8cd9d0b8
Mari <mrs.tereshchenko@gmail.com>
2023-01-30 16:13:41 +0100

    push task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,88 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Searchbar from './Searchbar/Searchbar';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ModalImage from './ImageGallery/ModalImage/ModalImage';
+import Button from './Shared/Button/Button';
+import Modal from './Shared/Modal/Modal';
+import Loader from './Shared/Loader/Loader';
+
+import { searchPics } from './Shared/PicsApi';
+
+class App extends Component {
+  state = {
+    search: '',
+    items: [],
+    loading: false,
+    error: null,
+    page: 1,
+    showModal: false,
+    modalImage: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { search, page } = this.state;
+    if (prevState.search !== search || prevState.page !== page) {
+      this.fetchPics();
+    }
+  }
+
+  async fetchPics() {
+    try {
+      this.setState({ loading: true });
+      const { search, page } = this.state;
+      const data = await searchPics(search, page);
+      this.setState(({ items }) => ({
+        items: [...items, ...data],
+      }));
+    } catch (error) {
+      this.setState({ error: error.message });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  searchPics = ({ search }) => {
+    this.setState({ search, items: [], page: 1 });
+  };
+  handleClick = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
+  };
+
+  openModal = ({ largeImageURL, tags }) => {
+    this.setState({
+      modalImage: {
+        largeImageURL,
+        tags,
+      },
+      showModal: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      showModal: false,
+    });
+  };
+
+  render() {
+    const { items, showModal, modalImage, loading, error } = this.state;
+    const { searchPics, handleClick, openModal, closeModal } = this;
+    return (
+      <>
+        <Searchbar onSubmit={searchPics} />
+        <ImageGallery items={items} openModal={openModal} />
+        {error && <p>{error}</p>}
+        {loading && <Loader />}
+        {Boolean(items.length) && <Button onClick={handleClick} />}
+        {showModal && (
+          <Modal close={closeModal}>
+            <ModalImage {...modalImage} />
+          </Modal>
+        )}
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+
+import styles from './searchbar.module.css';
+
+class Searchbar extends Component {
+  state = {
+    search: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit({ ...this.state });
+    this.reset();
+  };
+
+  reset() {
+    this.setState({
+      search: '',
+    });
+  }
+
+  render() {
+    const { search } = this.state;
+    const { handleChange, handleSubmit } = this;
+    return (
+      <header className={styles.searchbar}>
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+          <button className={styles.searchForm_button} type="submit">
+            <span className={styles.searchForm_button_label}>Search</span>
+          </button>
+          <input
+            className={styles.searchForm_input}
+            value={search}
+            onChange={handleChange}
+            name="search"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            required
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func,
+};
